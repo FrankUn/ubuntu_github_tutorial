9c6cdb9e431140a1aa0bb8fb8d521d18ede5829e
FrankUn <62336156+FrankUn@users.noreply.github.com>
2020-04-08 15:19:59 +0800

    Update test1.java
    a

--- test1.java
+++ test1.java
@@ -3,5 +3,6 @@ public class HelloWorld {
 	public static void main(String[] args)	{
 		//Prints "Test1.1" to the terminal window.
 		System.out.println("Test1.1");
+		System.out.println("Test1.2");
 	}
 }
